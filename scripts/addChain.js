4c4367fcab9305b6a11caa23ee6ef1502274ecc5
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-03-26 03:30:28 +0530

    edit scripts

--- scripts/addChain.js
+++ scripts/addChain.js
@@ -1,12 +1,19 @@
 const { ethers, network } = require("hardhat")
 
 async function addChain() {
-    if (network.config.chainId != 80001) {
+    if (network.config.chainId != 5) {
         console.log("Cross chain function cannot be called on this chain")
     } else {
-        const senderUNO = await ethers.getContract("OxSenderUNO")
-        await senderUNO.addChain("9991", "0x1e0Db00EB08ceC7FFdA03c0Dbf224193E1563844", "")
-        console.log("Entered!")
+        const senderUNO = await ethers.getContractAt(
+            "OxSenderUNO",
+            "0xF1D62f668340323a6533307Bb0e44600783BE5CA"
+        )
+        await senderUNO.addChain(
+            "9991",
+            "0x1e0Db00EB08ceC7FFdA03c0Dbf224193E1563844",
+            "0x878E67CAdEa753E407c812c76A15402912003e45"
+        )
+        console.log("Added the chain!")
     }
 }
 
